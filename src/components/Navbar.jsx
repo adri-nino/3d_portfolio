dbf38b8518aec3c90222b5119fed2bdeb2bcf2ba
Adriana Nino <adriananino1637@gmail.com>
2025-01-26 16:15:50 -0600

    adjusted flex for name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,9 +24,9 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-8 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Adriana Nino
-            <span className="sm:block hidden"> | Full Stack Developer</span>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Adriana Nino &nbsp;
+            <span className="sm:block hidden">| Full Stack Developer</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex gap-10">
